da0e1d25e4998ce0f78b1d46426013aedf58fc03
kriss <kriss@DESKTOP-7MMA02I>
2023-05-27 14:18:42 +0200

    Dodaj pliki projektów.

---
+++ ASPNET-MVC/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPNET_MVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET_MVC.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
